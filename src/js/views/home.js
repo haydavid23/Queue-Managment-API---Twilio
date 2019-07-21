bc47cfc8687c1746f197e5e0347fac9436bfc83d
David Hay <hay.david@hotmail.com>
2019-07-21 01:29:14 +0000

    tables

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,5 +1,7 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+
 import "../../styles/home.scss";
 
 export class Home extends React.Component {
@@ -11,9 +13,50 @@ export class Home extends React.Component {
 						<br />
 						<br />
 						<span>Waiting List </span>
+						<Table striped bordered hover>
+							<thead>
+								<tr>
+									<th>#</th>
+									<th>Customer Name</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>1</td>
+									<td>Mark</td>
+								</tr>
+								<tr>
+									<td>2</td>
+									<td>Jacob</td>
+								</tr>
+							</tbody>
+						</Table>
 					</div>
-					<div className="col-sm text-center">2 of 3</div>
-					<div className="col-sm text-center">3 of 3</div>
+					<div className="col-sm text-center m-auto">
+						<br />
+						<br />
+						<br />
+						<Button variant="success">Next</Button>
+					</div>
+					<div className="col-sm text-center">
+						<br />
+						<br />
+						<span>Being Process</span>
+						<Table striped bordered hover>
+							<thead>
+								<tr>
+									<th>#</th>
+									<th>Customer Name</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>1</td>
+									<td>Mark</td>
+								</tr>
+							</tbody>
+						</Table>
+					</div>
 				</div>
 			</div>
 		);
